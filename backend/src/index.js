093a432ae5f8fdc74bd2ad7af7e5c260a07d6cd4
Christian <alvescastro00cca13@gmail.com>
2020-08-30 20:57:39 -0300

    Métodos HTTP

---
+++ backend/src/index.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const { request, response } = require("express")
+
+const app = express()
+//MÉTODOS HTTP
+// 
+//GET: Buscar informações do back-end
+//POST: Criar uma informação no back-end
+//PUT: Alterar uma informação no back-end
+//DELETE: Deletar uma informação no back-end 
+
+app.get('/projects', (request, response) => {
+   return response.json([
+      "Projeto 1",
+      "Projeto 2"
+   ])
+})
+
+app.post('/projects', (request, response) => {
+   return response.json([
+      "Projeto 1",
+      "Projeto 2",
+      "Projeto 3"
+   ])
+})
+
+app.put('/projects/:id', (request, response) => {
+   return response.json([
+      "Projeto 4",
+      "Projeto 2",
+      "Projeto 3"
+   ])
+})
+
+app.delete('/projects/:id', (request, response) => {
+   return response.json([
+      "Projeto 2",
+      "Projeto 3"
+   ])
+})
+
+app.listen(3333, ()=>{
+   console.log("🚀 Back-end started!")
+})
